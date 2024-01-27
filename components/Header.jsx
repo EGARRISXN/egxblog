2955600be8b6d7fdba0408c8bfe1ae0fe51cce1f
EGARRISXN <egarrisxn@gmail.com>
2024-01-26 21:04:21 -0500

    added new link to personal site.

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,13 +3,22 @@ import { ThemeToggle } from "@/components/Theme-Toggle";
 
 export default function Header() {
   return (
-    <header className="max-w-3xl w-full flex mx-auto p-4 items-center justify-between border-b-2">
-      <Link href="/">
-        <h1 className="text-xl font-bold">
-          eg<span className="text-primary">xblog</span>
-        </h1>
-      </Link>
-      <ThemeToggle />
+    <header className="max-w-3xl flex justify-between mx-auto w-full p-4 border-b-2">
+      <div className="flex items-center">
+        <Link href="/">
+          <h1 className="text-xl font-bold text-foreground">
+            eg<span className="text-primary">xblog</span>
+          </h1>
+        </Link>
+      </div>
+      <div className="flex items-center gap-4">
+        <Link href="https://egxworld.net/">
+          <h1 className="text-xl hover:underline hover:underline-offset-4 hover:text-primary">
+            personal site
+          </h1>
+        </Link>
+        <ThemeToggle />
+      </div>
     </header>
   );
 }
