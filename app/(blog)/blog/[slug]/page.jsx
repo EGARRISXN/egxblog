eceb2d6d32821fbf5f6ec7b23e1f1dbeed53314c
EGARRISXN <egarrisxn@gmail.com>
2024-06-17 03:34:45 -0400

    added a blog!

--- app/(blog)/blog/[slug]/page.jsx
+++ app/(blog)/blog/[slug]/page.jsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import {format} from 'date-fns'
 import {client, urlFor} from '../../../../lib/sanityClient'
 import {Button} from '../../../../components/ui/button'
 import {PortableText} from '@portabletext/react'
@@ -10,7 +11,8 @@ async function getData(slug) {
   "currentSlug": slug.current,
     title,
     content,
-    coverImage
+    coverImage,
+    publishedAt
 }[0]`
 
   const data = await client.fetch(query)
@@ -32,9 +34,12 @@ export default async function BlogPost({params}) {
           <h2 className='block text-center text-xs font-semibold uppercase tracking-wide text-primary sm:text-base 2xl:text-lg'>
             "Spend a little time inside my head."
           </h2>
-          <h3 className='mt-4 block text-center text-xl font-bold leading-8 tracking-tight sm:text-2xl lg:text-3xl 2xl:text-4xl'>
+          <h3 className='mb-1 mt-3 block text-center text-2xl font-bold leading-8 tracking-tight sm:text-3xl lg:text-4xl 2xl:text-5xl'>
             {data.title}
           </h3>
+          <p className='font-semiboldd text-center text-xs tracking-wide sm:text-base 2xl:text-lg'>
+            {format(new Date(data.publishedAt), 'EEEE // MMMM dd yyyy')}
+          </p>
         </header>
         <figure>
           <Image
@@ -48,10 +53,14 @@ export default async function BlogPost({params}) {
         <div className='prose prose-lg prose-blue mx-auto mt-12 max-w-[800px] xl:prose-xl 2xl:prose-2xl dark:prose-invert prose-a:text-primary prose-li:marker:text-primary'>
           <PortableText value={data.content} />
         </div>
-        <Button asChild variant='destructive' className='mx-auto my-12 flex w-72 lg:w-80 2xl:w-96'>
+        <Button
+          asChild
+          variant='destructive'
+          className='mx-auto my-12 flex w-72 lg:w-80 xl:w-96 2xl:w-[32rem]'
+        >
           <Link href='/'>Return</Link>
         </Button>
-        <div className='prose prose-sm mx-auto my-12 w-fit rounded-lg border p-8 text-center text-foreground shadow lg:prose-lg xl:prose-xl lg:p-6 2xl:p-8'>
+        <div className='prose prose-sm mx-auto my-12 w-fit rounded-lg border p-8 text-center text-foreground shadow lg:prose-lg xl:prose-xl 2xl:prose-2xl lg:p-6 2xl:p-8'>
           If you'd like to support my work,
           <br />
           <a
@@ -60,8 +69,8 @@ export default async function BlogPost({params}) {
             rel='noopener noreferrer'
             className='text-foreground no-underline'
           >
-            consider buying me a coffee{' '}
-            <span className='text-green-400 no-underline hover:text-green-600'>here!</span>
+            consider buying me a{' '}
+            <span className='text-green-400 no-underline hover:text-green-600'>coffee!</span>
           </a>
         </div>
       </article>
